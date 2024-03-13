093185ea36cbcea1350176b4129d57b9be2470c2
thanh1149 <155382289+thanh1149@users.noreply.github.com>
2024-03-14 00:43:46 +0700

    eProject

--- SourceCode/home.php
+++ SourceCode/home.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Park</title>
+    <title>Merciado Amusement park</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* Style logo image */
         .navbar-brand img {
-            max-width: 100px;
+            max-width: 150px;
             /* Adjust according to your logo size */
             height: auto;
         }
@@ -17,11 +17,12 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img src="your_logo_image.jpg" alt="Logo"></a>
+        <a class="navbar-brand" href="#"><img src="./image/logo.jpg " alt="Logo"></a>
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
