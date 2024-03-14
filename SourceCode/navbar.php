4da6771bbc955eca148b6b39a80e4237e3205eb3
thanh1149 <155382289+thanh1149@users.noreply.github.com>
2024-03-15 00:36:33 +0700

    eProject

--- SourceCode/navbar.php
+++ SourceCode/navbar.php
@@ -1,59 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Merciado Amusement park</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        /* Style logo image */
-        .navbar-brand img {
-            max-width: 150px;
-            /* Adjust according to your logo size */
-            height: auto;
-        }
-    </style>
+    <link rel="stylesheet" href="navbar.css"/>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img src="./image/logo.jpg " alt="Logo"></a>
-        
+    <nav class="navbar navbar-expand-lg navbar-light border-bottom border-danger ">
+        <a class="navbar-brand" href="#"><img src="./image/logo.jpg " alt="Logo"></a> 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">HOME</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About us</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Experience
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Option 1</a>
-                        <a class="dropdown-item" href="#">Option 2</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Another Option</a>
-                    </div>
+                    <a class="nav-link" href="#">ABOUT US</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Event</a>
+                    <a class="nav-link" href="#">EXPERIENCE</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Entertainment</a>
+                    <a class="nav-link" href="#">EVENT</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Food</a>
+                    <a class="nav-link" href="#">ENTERTAINMENT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FOOD</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LOGIN/REGISTER</a>
                 </li>
             </ul>
         </div>
-    </nav>
+    </nav> 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
