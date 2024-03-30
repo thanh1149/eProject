a63d95dd60146268b69425e16a0c6aaf1bb623d9
thanh1149 <155382289+thanh1149@users.noreply.github.com>
2024-03-30 22:20:41 +0700

    eProject

---
+++ SourceCode/admin/admin-login.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . "/../function.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" >
+    <title>Admin login Form</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <form class="mx-auto">
+            <h4 class="text-center">Login</h4>
+            <div class="mb-3 mt-5">
+                <label for="name" class="form-label">Admin Name</label>
+                <input type="text" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary mt-5">Login</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+
+</html>
---
+++ SourceCode/admin/admin.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/../function.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN</title>
+</head>
+<body>
+    hi
+</body>
+</html>
