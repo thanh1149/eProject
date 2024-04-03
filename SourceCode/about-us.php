f84a07978f67b2b6a1ac2a63afc3447a94dce6a6
MinhNT2306L <164546894+MinhNT2306L@users.noreply.github.com>
2024-04-04 05:28:03 +0700

    eproject

---
+++ SourceCode/about-us.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/fe-style.css">
+</head>
+
+<body>
+    <?php include('nav+footer/navbar.php'); ?>
+    <div class="container">
+        <br>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb justify-content-center bg-transparent border-bottom-0">
+                <li class="breadcrumb-item"><a href="home.php" class="text-dark">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">About Us</li>
+            </ol>
+        </nav>
+        <br>
+        <img src="image/about_us/1.jpeg" alt="" style="width: 96%; max-height: 500px; display: block; margin: 0 auto;">
+        <br>
+        <br>
+        <div class="container-box">
+            <div class="row">
+                <div class="col-md-9">
+                    <section id="section1">
+                        <div>
+                            <p><b>
+                                    <h3>AISA PARK – A NEW WONDER OF ENDLESS EMOTIONS</h3>
+                                </b></p>
+                            <img src="image/about_us/2.jpeg" alt="" style="width: 100%; height: auto;">
+                            <br><br>
+                            <p>Aiming to commence a world-class amusement complex in Vietnam, Sun Group opted for Danang – a coastal tourist city on the narrow Central swathe of Viet Nam to be the site of a great amusement and entertainment park by the name of Asia Park.</p>
+                            <p>Spanning a total area of 868,694sqm on the western bank of Han River, Asia Park amusement park blends a string of unique and unseen features of entertainment models of the world and a ubiquitous touch of Oriental culture. Asia Park comprises three main zones: a modern outdoor park, a cultural park that consists of mini architectural works and artworks to symbolize 10 Asian countries, and the Sun Wheel – a hybrid ensemble of the old and the new.</p>
+                            <p><b>
+                                    <h3>THE LEGENDARY PLACE</h3>
+                                </b></p>
+                            <img src="image/about_us/3.jpeg" alt="" style="width: 100%; height: auto;">
+                            <br><br>
+                            <p><b>A famous tourist attraction in Da Nang</b></p>
+                            <p>The cultural park is a reenactment of the Oriental realm through replicas of diverse cultural landmarks, architectural and historical sites, art performances, and quintessential cuisines of 10 Asian countries, namely Japan, Indonesia, Singapore, Korea, India, Nepal, Thailand, Cambodia, China, and Vietnam.</p>
+                            <p><b>
+                                    <h3>EXCLUSIVE COMBO TICKET</h3>
+                                </b></p>
+                            <img src="image/about_us/4.jpeg" alt="" style="width: 100%; height: auto;">
+                            <br><br>
+                            <p>In addition to FREE ENTRY for all visitors, Asia Park also offers preferential tickets for visitors who want to experience unlimited services and high-class games, including:</p>
+                            <ul>
+                                <li><b><i>All in one Package ticket</i></b>for unlimited experience with all games will cost 200,000 VND/adult ticket; 100,000 VND/ticket for children 1m-1m4</li>
+                                <li><b><i>Ticket to experience Sun Wheel:</i></b>100,000 VND/1 adult; 50,000 VND / 1 child (1m-1m4) and free all games for children under 1m according to safety regulations.</li>
+                                <li><b><i>FEC Ticket:</i></b>Only from 35,000 VND/ 1 ticket (above 1m) and free all games for children under 1m according to safety regulations.</li>
+                                <li><b><i>Single ticket package: </i></b>Only from 50,000 VND, come to Asia Park at 20:00-21:45, you will have super reasonable fee:</li>
+                            </ul>
+                            <p>+ Adults: 50,000 VND/ 1 game turn; 100,000 / 3 games</p>
+                            <p>+ Children: 30,000 VND / 1 game turn; 60,000/3 games</p>
+                            </ul>
+                            <br>
+                            <img src="image/about_us/5.jpeg" alt="" style="width: 100%; height: auto;">
+                        </div>
+                        <br><br><br>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section id="section2">
+                        <h3 class="text-center">About Us</h3>
+                        <div class="box-container">
+                            <div class="box">
+                                <a href="experience.php">
+                                    <p class="text-center">Experience</p>
+                                    <img src="image/about_us/1.jpeg" alt="" class="img-fluid">
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="entertainment.php">
+                                    <p class="text-center">Entertainment</p>
+                                    <img src="image/about_us/2.jpeg" alt="" class="img-fluid">
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="restaurant.php">
+                                    <p class="text-center">Restaurant</p>
+                                    <img src="image/about_us/3.jpeg" alt="" class="img-fluid">
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="event.php">
+                                    <p class="text-center">Event</p>
+                                    <img src="image/about_us/4.jpeg" alt="" class="img-fluid">
+                                </a>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+    <?php include('nav+footer/footer.php'); ?>
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
