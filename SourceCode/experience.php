1659085fbaf5b5050aaa5a7f38a0409a22d768f0
MinhNT2306L <164546894+MinhNT2306L@users.noreply.github.com>
2024-04-04 05:28:57 +0700

    eproject

---
+++ SourceCode/experience.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Experience</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/fe-style.css">
+</head>
+
+<body>
+    <?php include('nav+footer/navbar.php'); ?>
+    <div class="container">
+        <br>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb justify-content-center">
+                <li class="breadcrumb-item"><a href="home.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Experience</li>
+            </ol>
+        </nav>
+        <br>
+        <div class="card-container">
+            <div class="card">
+                <img src="image/home/home 5.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">SINGAPORE ZONE</h5>
+                    <p class="card-text">MERLION ZONE With a height of 3.6m and weight of 10 tons The Merlion is a Singaporean mascot with the image of an animal with a lion head and a fish tail riding on...</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="image/home/home 6.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">NEPAL ZONE</h5>
+                    <p class="card-text">Nepal station: Nepal is considered a multicultural country with different religions such as Hinduism, Buddhism, Islam, Christianity etc. The hybrid culture between...</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="image/home/home 7.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">CAMBODIA ZONE</h5>
+                    <p class="card-text">THE DARK RIDE Meet the Mystery House inspired by traditional architectural features at Cambodian zone. The building amazes visitors by its mysterious appearance. No...</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="card-container">
+            <div class="card">
+                <img src="image/home/home 5.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">THAILAND ZONE</h5>
+                    <p class="card-text">Thailand Zone Description: The country of golden pagodas is recreated on the idea of a golden tower and the mythical mascot, Kinnara from Himapan Forest (belonging...</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="image/home/home 6.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">CHINA ZONE</h5>
+                    <p class="card-text">PANDA RESTAURANT Chinese style architecture With an area of 3241m2, including 4 buildings of restaurants and cafes. Accommodating about 1,000 seats, including 600...</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="image/home/home 7.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">INDIA ZONE</h5>
+                    <p class="card-text">India Zone: the observatory shelter and Namaste cultural stage can accommodate up to 2000 guests, which is an ideal place to organize events, large and small...</p>
+                    <a href="#" class="btn btn-primary">More</a>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br><br>
+    <?php include('nav+footer/footer.php'); ?>
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
